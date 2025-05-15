0e16b8941268f10b7a3fccdb85981bf7df32fe30
Sujan Maharjan <sujan.maharjan.1@ndsu.edu>
2025-05-15 17:43:56 -0500

    fix: added custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://sujanw0w.github.io/portfolio/",
-    base: "/",
+    site: "https://sujanmaharjan.name.np",
+    // base: "/portfolio",
 });
